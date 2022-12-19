d5a1ac8cf34d145c216ebc4e50904129e4ba4d60
daisuke-harada-sp <daisuke.harada@sailpoint.com>
2022-12-19 16:15:19 +0900

    update

---
+++ idn-rules/src/main/java/xxx/sailpoint/com/App.java
@@ -0,0 +1,17 @@
+package xxx.sailpoint.com;
+
+import sailpoint.object.Identity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        Identity identity = new Identity();
+    }
+}
